68db3dac3cd3708265a2546504191b5c53995401
Shruthic27 <76080986+Shruthic27@users.noreply.github.com>
2021-11-02 20:53:41 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var p=":"
+var up=""
+function ot12(){
+window.location="index2.html";
+}
+
+function btms(){
+window.location="index.html";
+}
+
+function li(){
+up=document.getElementById("up").value
+    if (up==p) {
+window.location="index3.html";
+}
+}
+
+function lo(){
+document.getElementById("name").innerHTML=" "
+document.getElementById("book").innerHTML=" "
+}
